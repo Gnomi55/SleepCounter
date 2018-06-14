b412d3bfc9ce7bdf495c6754d422d1a8ab4084c9
Gnomi55 <Florian@p200300c7e718ac000cedae50cb01876f.dip0.t-ipconnect.de>
2018-06-14 17:14:17 +0200

    Initial Commit

---
+++ SleepCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepCounter
+//
+//  Created by Florian Schweizer on 14.06.18.
+//  Copyright © 2018 Florian Schweizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
