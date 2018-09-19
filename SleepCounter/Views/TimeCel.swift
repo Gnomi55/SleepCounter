54bdf5561782e3d4ee0af79c59bfa9efc5ca9b0f
Florian <florian@florians-macbook-pro.local>
2018-09-19 16:15:50 -0300

    add refresh-button

---
+++ SleepCounter/Views/TimeCel.swift
@@ -0,0 +1,14 @@
+//
+//  TimeCel.swift
+//  SleepCounter
+//
+//  Created by Florian Schweizer on 11.07.18.
+//  Copyright © 2018 Florian Schweizer. All rights reserved.
+
+//
+
+import UIKit
+
+class TimeCell: UITableViewCell {
+    
+}
